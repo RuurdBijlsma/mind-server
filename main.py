4869a3252eb96a8ab71d88d833c93cfee3db5e54
Ruurd Bijlsma <ruurd@bijlsma.dev>
2020-04-05 17:44:03 +0200

    Add framework for cognitive model

--- main.py
+++ main.py
@@ -1,15 +1,87 @@
 import socketio
 import eventlet
+from aiohttp import web
+from model import Model
 
 # create a Socket.IO server
-sio = socketio.Server(cors_allowed_origins='*')
-app = socketio.WSGIApp(sio)
+sio = socketio.AsyncServer(cors_allowed_origins='*', logger=False)
+app = web.Application()
+sio.attach(app)
+# app = socketio.WSGIApp(sio)
+model = Model(sio)
+
+
+@sio.event
+def cards_played(sid, numbers):
+    print(f"Player played card(s) {numbers}")
+    model.update_player_hand_size(model.get_player_hand_size() - len(numbers))
+    # player can play more than one card when shuriken is played for example, or when they have consecutive cards
+    # In this card only regard the top card
+    # Tell model here (only need to update top card if the new card is higher than the current top card)
+    last_number_played = numbers[len(numbers) - 1]
+    if last_number_played > model.get_top_card():
+        model.update_top_card(last_number_played)
+
+
+@sio.event
+async def shuriken_proposed(sid):
+    print(f"Player proposed shuriken")
+    # Ask model for response here
+    response = model.get_shuriken_response()
+    await sio.emit('shuriken_vote', response)
+
+
+@sio.event
+def shuriken_vote(sid, vote, player_lowest_card):
+    if vote == 'no':
+        print(f"Player vetoed and denied our shuriken proposal: {vote}")
+        model.set_player_shuriken_response(False, None)
+    else:
+        # Also update shuriken amount left = shurikens_left - 1 in the next function
+        model.set_player_shuriken_response(True, player_lowest_card)
+        print(f"Player voted yes on our shuriken proposal, their lowest card = {player_lowest_card}")
+
+
+@sio.event
+def life_lost(sid):
+    print(f"Oh now we lost a life")
+    model.life_lost()
+
+
+@sio.event
+def update_model_hand(sid, new_hand):
+    print(f"Update model hand! Model's hand contains the cards: {new_hand}")
+    # This function is needed because the model hand can change (for example shuriken is played, 
+    # player shows lowest card, which is higher than cards in models hand.
+    # Then all cards in the models hand which are lower than the players lowest card are removed)
+    # It's also called at the start of each round
+    model.update_model_hand(new_hand)
+
+
+@sio.event  # Not sure this event is needed yet
+def update_player_hand_size(sid, player_hand_size):
+    print(f"Update player hand size: {player_hand_size}")
+    model.update_player_hand_size(player_hand_size)
+
+
+@sio.event
+def new_round(sid, new_hand):
+    print(f"New round: {len(new_hand)} reset model time and update hand to {new_hand}")
+    # Reset timer in new_round
+    model.update_player_hand_size(len(new_hand))
+    model.new_round(new_hand)
+
+
+@sio.event
+def new_game(sid):
+    print(f"New Game! reset everything")
+    # reset life count, etc.
+    model.new_game()
 
 
 @sio.event
 def connect(sid, environ):
     print('connect sid:', sid)
-    sio.emit("hello", "world")
 
 
 @sio.event
@@ -17,11 +89,5 @@ def disconnect(sid):
     print('disconnect sid:', sid)
 
 
-@sio.event
-def hello(sid, data):
-    print(f"Received hello event sid: {sid} data: {data}")
-    return "i received your hello :)"
-
-
 if __name__ == '__main__':
-    eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
+    web.run_app(app, port=5000)
---
+++ model.py
@@ -0,0 +1,114 @@
+from timer import Timer
+
+
+# None of this is final, the actual cognitive model needs to also be updated in all these functions
+
+
+class Model:
+    def __init__(self, sio):
+        self.shurikens_left = -1
+        self.lives_left = -1
+        self.hand = []
+        self.player_hand_size = -1
+        self.deck_top_card = -1
+        self.reset_game()
+        self.sio = sio
+
+        # Temp
+        self.timer = None
+
+    def get_top_card(self):
+        print("get_top_card", self.deck_top_card)
+        return self.deck_top_card
+
+    def update_top_card(self, new_top_card):
+        print("update_top_card", new_top_card)
+        self.deck_top_card = new_top_card
+        # Played played a card, wait for some seconds to maybe play model card?
+        self.temp_play_card_smart()
+
+    def temp_play_card_smart(self):
+        print("temp_play_card_smart")
+        # TEMPORARY (This just waits n seconds where n is the gap before playing lowest card)
+        if self.timer is not None:
+            self.timer.cancel()
+        if len(self.hand) == 0:
+            print("our hand is empty, no actions left to do")
+            return
+        lowest_card = self.get_lowest_card()
+        gap = lowest_card - self.get_top_card()
+        print(f"Waiting {gap} seconds before playing {lowest_card}")
+        self.timer = Timer(gap, self.play_lowest_card)
+
+    def get_lowest_card(self):
+        print("get_lowest_card")
+        if len(self.hand) == 1:
+            return self.hand[0]
+        if len(self.hand) == 0:
+            return False
+        return min(*self.hand)
+
+    async def play_lowest_card(self):
+        print("play_lowest_card")
+        lowest_card = self.get_lowest_card()
+        await self.play_card(lowest_card)
+
+    async def play_card(self, card):
+        print("play_card", card)
+        self.hand.remove(card)
+        await self.sio.emit('play_card', card)
+        self.update_top_card(card)
+
+    # noinspection PyMethodMayBeStatic
+    def get_shuriken_response(self):
+        print("get_shuriken_response")
+        return True
+
+    # noinspection PyMethodMayBeStatic
+    def set_player_shuriken_response(self, response, lowest_card):
+        print("set_player_shuriken_response")
+        pass
+
+    def life_lost(self):
+        print("life_lost")
+        self.lives_left -= 1
+
+    def get_player_hand_size(self):
+        print("get_player_hand_size")
+        return self.player_hand_size
+
+    def update_player_hand_size(self, new_hand_size):
+        print("update_player_hand_size")
+        self.player_hand_size = new_hand_size
+
+    def update_model_hand(self, new_hand):
+        print("update_model_hand", new_hand)
+        self.hand = new_hand
+        # Lowest card probably changed, to rethink our decisions
+        self.temp_play_card_smart()
+
+    def new_game(self):
+        print("new_game")
+        self.reset_game()
+
+    def new_round(self, new_hand):
+        print("new_round")
+        self.reset_timer()
+        self.reset_round()
+        self.update_model_hand(new_hand)
+
+    def reset_timer(self):
+        print("reset_timer")
+        pass
+
+    def reset_game(self):
+        print("reset_game")
+        self.shurikens_left = 3
+        self.lives_left = 3
+        self.reset_round()
+
+    def reset_round(self):
+        print("reset_round")
+        self.deck_top_card = 0
+        self.player_hand_size = 0
+        self.hand = []
