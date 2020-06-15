2d8dc7243e4dbbae2db6801fc17063a9271a0875
IDMAkrum <i.akrum@outlook.com>
2020-06-15 12:41:07 +0200

    Added timing data to model's memory. Data of how long to wait per gap stored in timing_data.csv. This is parsed into chunks for the model's declarative memory.

--- cogmodel.py
+++ cogmodel.py
@@ -1,6 +1,7 @@
 from actrmodel import ACTRModel
 from chunkCog import Chunk
-
+import pandas as pd
+import numpy as np
 
 # The cognitive part of the game's model
 
@@ -12,6 +13,7 @@ class CognitiveModel(ACTRModel):
 	# Initialise declarative memory with gap facts and pulse durations        
     def _init_memory(self):
     	_add_gap_facts(self)
+    	_add_wait_facts(self)
 
 	# Generate chunks for the gap facts and add them to memory    	
     def _add_gap_facts(self):
@@ -20,3 +22,14 @@ class CognitiveModel(ACTRModel):
     			gap_fact = Chunk(name = "gf-" + n1 + n2, slots = {"type": "gap-fact",
     				"num1":n1, "num2": n2, "gap":abs(n1-n2)})
     			self.add_encounter(gap_fact)
+
+   	# Generate chunks forthe wait facts and add them to memory
+   	def _add_wait_facts():
+   		# Get initial times to wait from timing_data csv
+   		data = pd.read_csv("timing_data.csv", usecols=['Gap','Pulses'])
+		array = data.to_numpy() # data is type numpy.int64 (compatible with int)
+   		for gap, time in array:
+   			chunk_name = "g" + str(gap) + "-w" + str(time)
+   			wait_fact = Chunk(name = chunk_name, slots = {"type": "wait-fact",
+   				"gap": gap, "wait": time})
+   			self.add_encounter(wait_fact)
